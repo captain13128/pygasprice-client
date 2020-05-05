dd6ad1017842bf7a1d23475ac355ff59ab7cb605
captain13128 <capitan.develop@gmail.com>
2020-05-05 14:18:28 +0300

    add setup.py

---
+++ setup.py
@@ -0,0 +1,17 @@
+from distutils.core import setup
+
+setup(
+    name='pygasprice-client',
+    version='1.0.0',
+    packages=[
+        'pygasprice_client',
+    ],
+    url='https://github.com/captain13128/pygasprice-client.git',
+    license='',
+    author='',
+    author_email='',
+    description='',
+    install_requires=[
+        'requests == 2.22.0',
+    ],
+)
